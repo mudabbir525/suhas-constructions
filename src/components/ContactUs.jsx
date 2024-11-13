48222313a121904ca6a135451485755a57fd2416
Mohammed Mudabbir Pasha <116947092+mudabbir525@users.noreply.github.com>
2024-11-14 01:16:12 +0530

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -64,7 +64,8 @@ const ContactUs = () => {
             {/* Embedded Map */}
 <div className="mt-8 h-48 bg-gray-200 rounded-lg overflow-hidden">
   <iframe
-    src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d942.0892414328906!2d78.57449584173608!3d17.327312768673348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPlot%20No.3%2C%20Road%20No.2%2C%20Andhrakeshari%20Nagar%2C%20Vanasthalipuram%2C%20Hyd-500%20070!5e1!3m2!1sen!2sin!4v1731526748108!5m2!1sen!2sin"
+    src="
+https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d942.0892414328906!2d78.57449584173608!3d17.327312768673348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1731527045708!5m2!1sen!2sin"
     width="100%"
     height="100%"
     allowFullScreen=""
